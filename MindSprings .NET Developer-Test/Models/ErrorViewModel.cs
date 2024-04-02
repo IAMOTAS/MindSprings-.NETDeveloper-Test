6270d97db8446e45646872a2a127d4fd5ff2a4ee
IAMOTAS <niiotasalokome@gmail.com>
2024-04-02 20:30:21 +0000

    Add project files.

---
+++ MindSprings .NET Developer-Test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MindSprings_.NET_Developer_Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
